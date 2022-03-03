51a210b95da141aca8862a349d9946b13923bfef
Austin White <austinlldb@gmail.com>
2022-03-02 18:02:41 -0700

    use a buffer to improve performance

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::env;
+use std::io::{self, BufWriter, Write};
+
+const BUFSIZE: usize = 8192;
 
 fn main() {
-  let expletive = env::args().nth(1).unwrap_or("y".into());
-  loop {
-    println!("{}", expletive);
-  }
+    let expletive = env::args().nth(1).unwrap_or("y".into());
+    let mut writer = BufWriter::with_capacity(BUFSIZE, io::stdout());
+    loop {
+        writeln!(writer, "{}", expletive).unwrap();
+    }
 }
